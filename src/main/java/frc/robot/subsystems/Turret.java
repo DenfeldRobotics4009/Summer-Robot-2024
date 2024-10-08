1701925471956aa14360075bd8bbef2126c00e51
Kingofhollows099 <kingofhollows099@gmail.com>
2024-10-08 16:49:57 -0500

    Worked on more functions for the turret class. All I need to finish is the setangle function. -T

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -9,33 +9,45 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
 
 public class Turret extends SubsystemBase {
   /** Creates a new Turret. */
   public CANSparkMax turretRotator = new CANSparkMax(11, MotorType.kBrushless); //make sure to find the actual deviceID
   public CANSparkMax invertedTurretRotator = new CANSparkMax(12, MotorType.kBrushless); //make sure to find the actual deviceID
   public CANSparkMax feeder = new CANSparkMax(10, MotorType.kBrushless); //make sure to find the actual deviceID
+
+  private PIDController rotatationPidController = new PIDController(0, 0, 0); //Makes a PID controller for turret rotation (put pid values in later)
+
   public Turret() {}
 
-  void manualrotate(){ //make this work
-
+  void rotate(double rotationSpeed){ 
+    //Rotate the turret with manual input
+    turretRotator.set(MathUtil.clamp(rotationSpeed, -1, 1));
+    invertedTurretRotator.set(MathUtil.clamp(rotationSpeed, -1, 1));
   }
   public void feed(){
-  //feeder motor runs
+    //feeder motor runs
     feeder.set(Constants.Turret.feederSpeed);
   }
   public void reversefeed(){
-  //feeder motor runs opposite
+    //feeder motor runs opposite
     feeder.set(Constants.Turret.reverseFeederSpeed);
   }
   void holdfeeder(){
-  //holds feeder motor in place
+    //holds feeder motor in place
     feeder.set(Constants.Turret.staticMotor);
   }
-  void turretlock(){
-  //Sets turret rotators to 0 
+  public void turretlock(){
+    //Locks the turret's rotating motors in place
+    turretRotator.set(Constants.Turret.staticMotor);
+    invertedTurretRotator.set(Constants.Turret.staticMotor);
   }
-  void setangle(){}
+  public void setangle(double targetAngle){
+    //Allows you set a certain rotation angle in your code
+    
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
